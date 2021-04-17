5306e0c81ca2c97c0791b13e8248ba8d78dfd7ac
nguyen khai <nnkhai10@outlook.com>
2021-04-17 22:59:28 +0700

    Add project files.

---
+++ ConsoleApplication/CoreTypeSystem.fs
@@ -0,0 +1,113 @@
+﻿module CoreTypeSystem
+open Microsoft.FSharp.Collections
+type Tag = 
+    | Plus = 0
+    | Minus = 1
+    | Max = 2
+    | Join = 3
+    | New = 4
+type TagNum = Tag * int
+type TagSeq = TagNum list
+type Term = 
+    | Branch of Term list
+    | Leaf of TagNum
+// Define seq function
+let rec seq (lst : TagSeq) : TagSeq = 
+    match lst with
+    | [] -> []
+    | (Tag.Minus, 0) :: xs -> seq xs
+    | (Tag.New, n1) :: (Tag.New, n2) :: xs -> seq ((Tag.New, n1 + n2) :: xs)
+    | (Tag.Plus, n1) :: (Tag.New, n2) :: xs -> seq ((Tag.Plus, n1 + n2) :: xs)
+    | (Tag.Plus, n1) :: (Tag.Max, n) :: (Tag.Minus, n2) :: xs -> seq ((Tag.Max, n1 + n) :: (Tag.Minus, n2 - 1) :: xs)
+    | (Tag.Plus, n1) :: (Tag.Max, n2) :: (Tag.New, n3) ::(Tag.Minus, n4) :: xs -> seq ((Tag.Max, n1 + n2)::(Tag.Max, n1 + n3) :: (Tag.Minus, n4 - 1) :: xs)
+    | (Tag.Max, n1) :: (Tag.Max, n2) :: xs -> seq ((Tag.Max, max n1 n2) :: xs)
+    | (Tag.Minus, n1) :: (Tag.Minus, n2) :: xs -> seq ((Tag.Minus, n1 + n2) :: xs)
+    | (Tag.Plus, n1) :: (Tag.Minus, n2) :: xs -> seq ((Tag.Max, n1) :: (Tag.Minus, n2 - 1) :: xs)
+    | x :: xs -> x :: (seq xs)
+// Define join function
+let rec join (lst : TagSeq) : TagSeq = 
+    match lst with
+    | [] -> []
+    | (Tag.Minus, n) :: xs -> 
+        if n > 0 then (Tag.Join, 1) :: (join ((Tag.Minus, n - 1) :: xs))
+        else (join xs)
+    | x :: xs -> x :: (join xs)
+// Define merge function
+let rec merge (lst1 : TagSeq) (lst2 : TagSeq) : TagSeq = 
+    match lst1 with
+    | [] -> lst2
+    | (Tag.Max, n1) :: xs1 ->
+        match lst2 with
+        | [] -> lst1
+        | (Tag.Max, n2) :: xs2 ->
+            (Tag.Max, n1 + n2) :: (merge (xs1) (xs2))
+        | (Tag.Join, n2) :: xs2 ->
+            (merge (lst1) ((Tag.Max, 0)::lst2))
+        | otherwise -> failwith "ERROR in merge 1"
+    | (Tag.Join, n1) :: xs1 ->
+        match lst2 with
+        | [] -> lst1
+        | (Tag.Join, n2) :: xs2 ->
+            (Tag.Join, n1 + n2) :: (merge (xs1) (xs2))
+        | (Tag.Max, n2) :: xs2 ->
+            (merge ((Tag.Max, 0)::lst1) (lst2))
+        | otherwise -> failwith "ERROR in merge 2"
+    | otherwise -> failwith "ERROR in merge 3"
+// Define joint commit function
+let rec jc (lst1 : TagSeq) (lst2 : TagSeq) : TagSeq = 
+    let revlst1 = List.rev lst1    //Reverse the string lst1
+    match revlst1 with
+    | [] -> lst2
+    | (Tag.Max, n') :: (Tag.Plus, n) :: xs1 -> 
+        match lst2 with
+        | [] -> lst1
+        | (Tag.Max, l') :: (Tag.Join, l) :: xs2 -> 
+            jc (seq (List.append (List.rev xs1) [ (Tag.Max, n + n') ])) (seq ((Tag.Max, l' + l * n) :: xs2))
+        | (Tag.Join, l) :: xs2 -> jc lst1 ((Tag.Max, 0) :: lst2)
+        | otherwise -> failwith "ERROR jc 1: mismatch."
+    | (Tag.Plus, n) :: xs1 -> jc (List.append lst1 [ (Tag.Max, 0) ]) lst2
+    | (Tag.Max, n') :: [] -> 
+        match lst2 with
+        | [] -> lst1
+        | (Tag.Max, l') :: xs2 -> seq ((Tag.Max, max n' l') :: xs2)
+        | (Tag.Join, l) :: xs2 -> seq ((Tag.Max, n') :: lst2)
+        | otherwise -> failwith "ERROR jc 2: mismatch."
+    | otherwise -> failwith "ERROR jc 3."
+
+// Define choice function
+//let choice (lst1 : TagSeq) (lst2 : TagSeq) : TagSeq = 
+//    match lst1 with
+//    | [] -> 
+//        if List.isEmpty lst2 then []
+//        else lst2
+//    | x :: xs -> 
+//        match lst2 with
+//        | [] -> lst1
+//        | x2 :: xs2 -> 
+//            if (fst x) = (fst x2) && (xs = xs2) && (fst x) = Tag.Max then 
+//                if (snd x >= snd x2) then lst1
+//                else lst2
+//            else []
+
+let rec infer (branch : Term list) (headseq : TagSeq) = 
+    match branch with
+    | [] -> seq headseq
+    | x :: xs -> 
+        match x with
+        | Leaf tagnum -> 
+            let newhead = seq (List.append headseq [ tagnum ])
+            infer xs newhead
+        | Branch br -> 
+            let child = join (infer br [])
+            let parent = join (infer xs [])
+            let tailseq = seq (merge child parent)
+            seq (jc headseq tailseq)
+
+//let rec addTree (lst1 : char list) : Term list = 
+//    match lst1 with
+//    | [] -> []
+//    | '-' :: xs -> List.append [ Leaf(Tag.Minus, 1) ] (addTree xs)
+//    | '+' :: xs -> List.append [ Leaf(Tag.Plus, 1) ] (addTree xs)
+//    | '(' :: xs -> [ Branch(addTree xs) ]
+//    | ')' :: xs -> addTree xs
+//    | x :: xs -> addTree xs
---
+++ ConsoleApplication/UnitTests.fs
@@ -0,0 +1,213 @@
+﻿open System.IO
+open CoreTypeSystem
+open NUnit.Framework
+open FsUnit
+
+// Change TagSeq list to TFJ string
+let rec ToString(lst : TagSeq) : string = 
+    match lst with
+    | [] -> ""
+    | (Tag.Plus, n1) :: xs -> "+" + n1.ToString() + ToString xs
+    | (Tag.Minus, n1) :: xs -> "-" + n1.ToString() + ToString xs
+    | (Tag.Max, n1) :: xs -> "#" + n1.ToString() + ToString xs
+    | (Tag.Join, n1) :: xs -> ":" + n1.ToString() + ToString xs
+    | (Tag.New, n1) :: xs -> "*" + n1.ToString() + ToString xs
+    | x :: xs -> x.ToString() + ToString xs
+
+let test ast expectedResult =
+    let res = infer ast [] in
+        ToString res |> should equal expectedResult
+
+///+0*1+0*4-1*4-1 -> #5
+let ast14 = 
+    [ Leaf(Tag.Plus, 0)
+      Leaf(Tag.New, 1)
+      Leaf(Tag.Plus, 0)
+      Leaf(Tag.New, 4)
+      Leaf(Tag.Minus, 1)
+      Leaf(Tag.New, 4)
+      Leaf(Tag.Minus, 1) ] in 
+      test ast14 "#5"
+
+//+2*4-1 -> #6
+let ast1 = 
+    [ Leaf(Tag.Plus, 2)
+      Leaf(Tag.New, 4)
+      Leaf(Tag.Minus, 1) ] in 
+      test ast1 "#6"
+
+//+0*1+2*1-1-1 -> #4
+let ast2 = 
+    [ Leaf(Tag.Plus, 0)
+      Leaf(Tag.New, 1)
+      Leaf(Tag.Plus, 2)
+      Leaf(Tag.New, 1)
+      Leaf(Tag.Minus, 1)
+      Leaf(Tag.Minus, 1) ]in 
+      test ast2 "#4"
+
+//+2(-1)-1 -> #4
+let ast3 = 
+    [ Leaf(Tag.Plus, 2)
+      Branch([ Leaf(Tag.Minus, 1) ])
+      Leaf(Tag.Minus, 1) ] in 
+      test ast3 "#4"
+
+//+2+3-1(-1)-1 -> #5
+let ast4 = 
+    [ Leaf(Tag.Plus, 2)
+      Leaf(Tag.Plus, 3)
+      Leaf(Tag.Minus, 1)
+      Branch([ Leaf(Tag.Minus, 1) ])
+      Leaf(Tag.Minus, 1) ]in 
+      test ast4 "#5"
+
+//+2(+3*1-1-1)-1 -> #8
+let ast5 = 
+    [ Leaf(Tag.Plus, 2)
+      Branch([ Leaf(Tag.Plus, 3)
+               Leaf(Tag.New, 1)
+               Leaf(Tag.Minus, 1)
+               Leaf(Tag.Minus, 1) ])
+      Leaf(Tag.Minus, 1) ]in 
+      test ast5 "#8"
+
+//+2(+2-2)-1 -> #6
+let ast6 = 
+    [ Leaf(Tag.Plus, 2)
+      Branch([ Leaf(Tag.Plus, 2)
+               Leaf(Tag.Minus, 2) ])
+      Leaf(Tag.Minus, 1) ]in 
+      test ast6 "#6"
+
+//+2(+1-1-1)-1 -> #5
+let ast7 = 
+    [ Leaf(Tag.Plus, 2)
+      Branch([ Leaf(Tag.Plus, 1)
+               Leaf(Tag.Minus, 1)
+               Leaf(Tag.Minus, 1) ])
+      Leaf(Tag.Minus, 1) ] in 
+      test ast7 "#5"
+
+//+1+2-1(-1)-1 -> #3
+let ast8 = 
+    [ Leaf(Tag.Plus, 1)
+      Leaf(Tag.Plus, 2)
+      Leaf(Tag.Minus, 1)
+      Branch([ Leaf(Tag.Minus, 1) ])
+      Leaf(Tag.Minus, 1) ] in 
+      test ast8 "#3"
+
+//+1+2(+4-1-1-1)+3(+5-1-1-1-1)-1+6-1-1+7-1-1
+let ast9 = 
+    [ Leaf(Tag.Plus, 1)
+      Leaf(Tag.Plus, 2)
+      Branch([ Leaf(Tag.Plus, 4)
+               Leaf(Tag.Minus, 1)
+               Leaf(Tag.Minus, 1)
+               Leaf(Tag.Minus, 1) ])
+      Leaf(Tag.Plus, 3)
+      Branch([ Leaf(Tag.Plus, 5)
+               Leaf(Tag.Minus, 1)
+               Leaf(Tag.Minus, 1)
+               Leaf(Tag.Minus, 1)
+               Leaf(Tag.Minus, 1) ])
+      Leaf(Tag.Minus, 1)
+      Leaf(Tag.Plus, 6)
+      Leaf(Tag.Minus, 1)
+      Leaf(Tag.Minus, 1)
+      Leaf(Tag.Plus, 7)
+      Leaf(Tag.Minus, 1)
+      Leaf(Tag.Minus, 1) ] in 
+      test ast9 "#24"
+
+//+1+2(+4-1-1-1)+3(+5-1-1-1-1)-1+12-1-1+7-1-1
+let ast10 = 
+    [ Leaf(Tag.Plus, 1)
+      Leaf(Tag.Plus, 2)
+      Branch([ Leaf(Tag.Plus, 4)
+               Leaf(Tag.Minus, 1)
+               Leaf(Tag.Minus, 1)
+               Leaf(Tag.Minus, 1) ])
+      Leaf(Tag.Plus, 3)
+      Branch([ Leaf(Tag.Plus, 5)
+               Leaf(Tag.Minus, 1)
+               Leaf(Tag.Minus, 1)
+               Leaf(Tag.Minus, 1)
+               Leaf(Tag.Minus, 1) ])
+      Leaf(Tag.Minus, 1)
+      Leaf(Tag.Plus, 12)
+      Leaf(Tag.Minus, 1)
+      Leaf(Tag.Minus, 1)
+      Leaf(Tag.Plus, 7)
+      Leaf(Tag.Minus, 1)
+      Leaf(Tag.Minus, 1) ] in 
+      test ast10 "#25"
+
+//+1+2(+1-1-1-1)+3(+1-1-1-1-1)-1+6-1-1+15-1-1
+let ast11 = 
+    [ Leaf(Tag.Plus, 1)
+      Leaf(Tag.Plus, 2)
+      Branch([ Leaf(Tag.Plus, 1)
+               Leaf(Tag.Minus, 1)
+               Leaf(Tag.Minus, 1)
+               Leaf(Tag.Minus, 1) ])
+      Leaf(Tag.Plus, 3)
+      Branch([ Leaf(Tag.Plus, 1)
+               Leaf(Tag.Minus, 1)
+               Leaf(Tag.Minus, 1)
+               Leaf(Tag.Minus, 1)
+               Leaf(Tag.Minus, 1) ])
+      Leaf(Tag.Minus, 1)
+      Leaf(Tag.Plus, 6)
+      Leaf(Tag.Minus, 1)
+      Leaf(Tag.Minus, 1)
+      Leaf(Tag.Plus, 15)
+      Leaf(Tag.Minus, 1)
+      Leaf(Tag.Minus, 1) ] in 
+      test ast11 "#18"
+
+//+0*4*4(+0+0*4-1-1-1)(+0-1+0-1-1+0*4*4*4-1) ->#20
+let ast12 = 
+    [ Leaf(Tag.Plus, 0)
+      Leaf(Tag.New, 4)
+      Leaf(Tag.New, 4)
+      Branch([ Leaf(Tag.Plus, 0)
+               Leaf(Tag.Plus, 0)
+               Leaf(Tag.New, 4)
+               Leaf(Tag.Minus, 1)
+               Leaf(Tag.Minus, 1)
+               Leaf(Tag.Minus, 1)])
+      Leaf(Tag.Plus, 0)
+      Leaf(Tag.Minus, 1)
+      Leaf(Tag.Plus, 0)
+      Leaf(Tag.Minus, 1)
+      Leaf(Tag.Minus, 1)
+      Leaf(Tag.Plus, 0)
+      Leaf(Tag.New, 4)
+      Leaf(Tag.New, 4)
+      Leaf(Tag.New, 4)
+      Leaf(Tag.Minus, 1) ] in 
+      test ast12 "#20"
+
+//+0*4(+0*4*4-1-1)(+0*4+0*4*4-1-1-1)->28
+let ast13 = 
+    [ Leaf(Tag.Plus, 0)
+      Leaf(Tag.New, 4)
+      Branch([ Leaf(Tag.Plus, 0)
+               Leaf(Tag.New, 4)
+               Leaf(Tag.New, 4)
+               Leaf(Tag.Minus, 1)
+               Leaf(Tag.Minus, 1)])
+      Leaf(Tag.Plus, 0)
+      Leaf(Tag.New, 4)
+      Leaf(Tag.Plus, 0)
+      Leaf(Tag.New, 4)
+      Leaf(Tag.New, 4)
+      Leaf(Tag.Minus, 1)
+      Leaf(Tag.Minus, 1)
+      Leaf(Tag.Minus, 1) ] in 
+      test ast13 "#28"
+
+printfn "\nAll tests were executed. Press a key to exit.\n"
+System.Console.ReadKey(true) |> ignore
